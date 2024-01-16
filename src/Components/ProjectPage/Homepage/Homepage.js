9ffae752bbe5f11dccdaab5093193641ffff6847
LenaChu <a87192611@gmail.com>
2024-01-15 22:26:58 -0600

    wip

--- src/Components/ProjectPage/Homepage/Homepage.js
+++ src/Components/ProjectPage/Homepage/Homepage.js
@@ -4,28 +4,64 @@ import { content } from "./consts";
 export default function Homepage() {
   return (
     <main>
-      <div className="project-title">
+      <section className="sec project-title">
         <h1 className="display-4">{content.heroSection.title}</h1>
         <h5>{content.heroSection.statement}</h5>
         <img className="bg-image 1" alt="user homepage mockup"></img>
         <img className="bg-image 2" alt="user homepage mockup"></img>
-      </div>
-      <div className="client-intro">
+      </section>
+      <section className="sec client-intro">
         <div className="col-1">
           <div className="intro">
             <h2>{content.client.title}</h2>
             <p>{content.client.description}</p>
           </div>
+          <div className="primary-users">
+            <h3>Primary Users</h3>
+            <p>{content.client.users.primary}</p>
+          </div>
+          <div className="secondary-users">
+            <h3>Secondary Users</h3>
+            <p>{content.client.users.secondary}</p>
+          </div>
         </div>
-        <div className="col-2"></div>
-      </div>
-      <h1>{content.overview.title}</h1>
-      <p>{content.overview.description}</p>
-      <p>{content.overview.timeline}</p>
-      <ul>
-        <li>{content.kickoff.list.bullet1}</li>
-        <li>{content.kickoff.list.bullet2}</li>
-      </ul>
+        <div className="col-2">
+          <img className="tg-logo" alt="TalentGuard Logo"></img>
+        </div>
+      </section>
+      <section className="sec project-overview">
+        <div className="col-1">
+          <div className="intro">
+            <h2>{content.overview.title}</h2>
+            <p>{content.overview.description}</p>
+          </div>
+          <div className="breakdown">
+            <div className="responsibilities">
+              <h3>My Responsibilities</h3>
+              <p>{content.overview.breakdown.responsibilities}</p>
+            </div>
+            <div className="challenges">
+              <h3>Challenges</h3>
+              <p>{content.overview.breakdown.challenges}</p>
+            </div>
+            <div className="members">
+              <h3>Team Members</h3>
+              <p>{content.overview.breakdown.members}</p>
+            </div>
+            <div className="delivery">
+              <h3>Final Delivery</h3>
+              <p>{content.overview.breakdown.delivery}</p>
+            </div>
+            <div className="timeline">
+              <h3>Timeline</h3>
+              <p>{content.overview.breakdown.timeline}</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-2">
+          <img className="old-homepage" alt="TalentGuard Old Homepage"></img>
+        </div>
+      </section>
     </main>
   );
 }
--- src/Components/ProjectPage/Homepage/consts.js
+++ src/Components/ProjectPage/Homepage/consts.js
@@ -7,19 +7,114 @@ export const content = {
     title: "Who is TalentGuard?",
     description:
       "TalentGuard is a HR software startup. They provide products for performance review, OKRs, skills assessments and development, career paths.",
+    users: {
+      primary: "Employees and managers in organizations",
+      secondary:
+        "Executive and HR administrators (Less than 10% of the total users.)",
+    },
   },
   overview: {
     title: "Project Overview",
     description:
-      "TalentGuard’s user homepage had been complained about not being helpful to users to start using this platform. Sales were also losing deals from the competitors with prospect clients commenting the homepage was not appealing. There was a strong voice to redesign it to address both the needs for users and the business.",
-    timeline: "15 weeks",
+      "TalentGuard’s user homepage had been complained about not being helpful to users to start using this platform. Sales were also losing deals from the competitors with prospect clients commenting the homepage was not appealing. There was a strong voice to redesign it to address both the needs for the users and the business.",
+    breakdown: {
+      responsibilities:
+        "User research, stakeholders management, UI design, user stories, design QA",
+      members:
+        "A Product manager, a technical writer, five full-stake developers, a QA engineer",
+      timeline: "April 2022 - August 2022 (15 weeks)",
+      challenges:
+        "Short deadline, content restriction, and limited access to end-users",
+      delivery: "MVP of the user homepage",
+    },
+  },
+  problems: {
+    first: "New users didn’t know how to start using it.",
+    second: "Existing users struggled to find tasks to do.",
+    third: "Sales were losing deals to competitors.",
+    forth: "CS were burned out due to required training.",
+  },
+  hmw: {
+    statement:
+      "design a new user homepage to help users adopt the platform and drive business growth? ",
   },
   kickoff: {
     title: "Project Kickoff & Insights",
-    description: "Lorem Ipsum",
-    list: {
-      bullet1: "Point 1",
-      bullet2: "Point 2",
+    description:
+      "We conducted a series of research to understand the problem space and defined the scope of the project. I further conducted a competitors analysis to understand TalentGuard’s market positioning.",
+    insights: {
+      stakeholderInterviews:
+        "Stakeholders had expectations in different levels: from solving users burning needs to re-visioning TalentGuard’s brand.",
+      userSurvey:
+        "Users cared more about tasks management and helpful tips than engaging features.",
+    },
+    decisions: {
+      expectations:
+        "Communicating the priority to align with business goals is essential for scoping the project.",
+      step: "Solving the primary pain points can bring fast wins to users and the business than having a major transformation.",
+    },
+    competitors: {
+      title: "TalentGuard’s homepage should support better task management.",
+      description:
+        "Competitors analysis revealed that TalentGuard leaned on the side emphasizing workforce management, and had great needs for curating outstanding tasks.",
+      categories: {
+        management:
+          "Featured with performance review, OKRs, and skills assessments.",
+        development:
+          "Featured with talent marketplace, employee upskill, employee growth and retention.",
+      },
+    },
+  },
+  solutions: {
+    first: "A personal and friendly landing experience.",
+    second: "Visibility of progress and outstanding tasks.",
+    third: "Flexibility to customize based on interests.",
+  },
+  feature1: {
+    title: "Welcome Section",
+    highlight1: {
+      title: "Highlight user identity at the first glance.",
+      description:
+        "A mini profile to bring some personal touch and a prompt for completing user profile.",
+    },
+    highlight2: {
+      title: "Greet users with warmth and helpful info.",
+      description:
+        "A greeting section feeding articles from our knowledge base.",
+    },
+  },
+  feature2: {
+    title: "Module Status + Call to Actions",
+    highlight1: {
+      title: "Inform users status of progress",
+      description: "A small widget showing overall status/progress.",
+    },
+    highlight2: {
+      title: "Promote the efficiency of task prioritizing.",
+      description: "A large widget showing outstanding tasks and actions.",
+    },
+  },
+  feature3: {
+    title: "Content and Layout Customization",
+    highlight1: {
+      title: "Flexibility to change the layout.",
+      description:
+        "The positions of the widgets can be switched by drag and drop.",
+    },
+    highlight2: {
+      title: "Freedom to configure content display.",
+      description:
+        "A customization panel allows users to reorder and hide/display modules on the homepage.",
+    },
+  },
+  impacts: {
+    description:
+      "The new user homepage received positive feedback from both end-users and prospective clients.",
+    next: {
+      title: "Next phase, enhance the content and continue optimization.",
+      bullet1: "Enrich data insights of the modules.",
+      bullet2: "Serve up more personalized content.(recommended skills, etc.)",
+      bullet3: "Measure the click rate of CTAs.",
     },
   },
 };
